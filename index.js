e729f268414a74798a75049e8e4efcfa9ffad240
Luke Austin <68570779+zokluke@users.noreply.github.com>
2021-02-10 16:39:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,26 @@ const server = require('server');
 const { get, socket } = server.router;
 const { render } = server.reply;
 
+const updateCounter = ctx => {
+  ctx.io.emit('count', ctx.io.sockets.sockets.length);
+};
+
+server([
+  // For the initial load render the index.html
+  get('/', ctx => render('index.html')),
+
+  // Join/leave the room
+  socket('connect', updateCounter),
+  socket('disconnect', updateCounter)
+]);
+Finally let's create a new socket router that, when it receives a message, it will push the same message to everyone in a similar way as before:
+
+// /index.js
+
+const server = require('server');
+const { get, socket } = server.router;
+const { render } = server.reply;
+
 // Update everyone with the current user count
 const updateCounter = ctx => {
   ctx.io.emit('count', Object.keys(ctx.io.sockets.sockets).length);
